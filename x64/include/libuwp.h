fae3421e5fa8cce5d4955cb2027a8a9f802adc8e
= <danworley@gmail.com>
2026-05-10 12:07:18 -0400

    debugging

---
+++ x64/include/libuwp.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#define LIBAPI extern "C" __declspec(dllexport)
+
+// :: Screen information
+LIBAPI void  uwp_GetActualSize(int* x, int* y); // Gets the display resolution
+LIBAPI void  uwp_GetScreenSize(int* x, int* y); // Gets the display resolution OR override from user
+LIBAPI float uwp_GetRefreshRate();
+LIBAPI void* uwp_GetActualWindowReference();
+LIBAPI void* uwp_GetWindowReference();
+
+// By default GetScreenSize will return display resolution, but this will allow apps to override those values
+LIBAPI void  uwp_SetScreenSize(int x, int y);
+
+// :: Filepaths
+LIBAPI void uwp_GetBundlePath(char* buffer);
+LIBAPI void uwp_GetBundleFilePath(char* buffer, const char* filename);
+
+// :: Events
+
+// If not using SDL or other helper you must occasionally call this to get anything to show on screen
+LIBAPI void uwp_ProcessEvents();
+
+// If not using SDL or other helper you must register event callbacks to read controller input
+LIBAPI void uwp_RegisterGamepadCallbacks(void (*callback)(void));
+
+// :: Basic app support
+extern int (*UWPAppEntryPoint)();
+LIBAPI int uwp_main(int mainFunc());
